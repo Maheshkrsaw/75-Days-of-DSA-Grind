ba8ab624ef65899e96ae71aab8bdcdbf179877cc
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-08 13:58:13 +0530

    completed the second largeds no with corner cases

--- DAY03/index.js
+++ DAY03/index.js
@@ -3,18 +3,26 @@ function secondlargest(arr){
     let firstlargest=-Infinity;
     let secondlargest=-Infinity;
 
+    if(arr.length<2){
+        return null;
+    }
+
+    if(arr.value==secondlargest){
+
+    }
+
     for (let i=0 ; i<arr.length ; i++){
         if(arr[i]>firstlargest){
             secondlargest=firstlargest;
             firstlargest=arr[i];
         }
-        else if(arr[i]>secondlargest){
+        else if(arr[i]>secondlargest && arr[i]!=firstlargest){
             secondlargest=arr[i];
         }
     }
     return secondlargest;
 }
 
-let arr=[14,19,0,2,8,5,6];
+let arr=[15,20,20,20,10,15];
 let results=secondlargest(arr);
 console.log(results);
