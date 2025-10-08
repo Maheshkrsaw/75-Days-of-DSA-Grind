6327903e4a36e7aae2eae5bfd50d25aa4a42650c
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-08 18:57:05 +0530

    updating

---
+++ 04-STAR-PATTERN/index.js
@@ -0,0 +1,20 @@
+/*
+print this line of code 
+* * * * 
+* * * * 
+* * * * 
+* * * *
+
+*/
+
+
+
+let n=4;
+for (let i=0;i<n;i++){
+    let row="";
+for (let j=0;j<n;j++){
+row=row+"* "
+}
+console.log(row);
+
+}
