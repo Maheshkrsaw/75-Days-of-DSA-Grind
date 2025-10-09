fdaaca810d0f96d6575820f8b067f163eaddc924
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-09 19:07:40 +0530

    updating

--- 04-STAR-PATTERN/index.js
+++ 04-STAR-PATTERN/index.js
@@ -52,12 +52,41 @@ q....)
 */
 
 
+// for (let i=0;i<=4;i++){
+//     let row="";
+// for (let j=0;j<=i;j++){
+// row= row +" " + ( j+1);
+// }
+// console.log(row);
+
+// }
+
+
+/*
+ 1
+ 2 2
+ 3 3 3
+ 4 4 4 4
+ 5 5 5 5 5
+ */
+
+// for (let i=0;i<=4;i++){
+//     let row="";
+// for (let j=0;j<=i;j++){
+// row= row +" " + ( i+1);
+// }
+// console.log(row);
+
+// }
+
+
+
+
 for (let i=0;i<=4;i++){
     let row="";
 for (let j=0;j<=i;j++){
-row= row +" " + ( j+1);
+row= row +" " + ( i+1);
 }
 console.log(row);
 
 }
-
