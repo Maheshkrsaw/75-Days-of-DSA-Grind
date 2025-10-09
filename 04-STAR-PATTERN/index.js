04090ddd0bb65864784f2f703926b88fc6adf963
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-09 19:56:09 +0530

    updating

--- 04-STAR-PATTERN/index.js
+++ 04-STAR-PATTERN/index.js
@@ -78,10 +78,25 @@ q....)
  1
  */
 
-for (let i = 0; i < 5; i++) {
-  let row = "";
-  for (let j = 1; j <= 5 - i; j++) {
-    row = row + " " + j;
-  }
-  console.log(row);
+// for (let i = 0; i < 5; i++) {
+//   let row = "";
+//   for (let j = 1; j <= 5 - i; j++) {
+//     row = row + " " + j;
+//   }
+//   console.log(row);
+// }
+
+
+
+let n=5
+for (let i = 0; i < n; i++) {
+    let row="";
+    for (let j=0 ; j<(n-(i+1));j++){
+        row=row+" ";
+    }
+    for (let k=0; k<(i+1);k++){
+        row=row+"*";
+    }
+    console.log(row);
+    
 }
