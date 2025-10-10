fef33897be679da99cc407ff502430b37055e8c5
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-10 12:48:32 +0530

    updating

--- 05-COUNTDIGIT,PALINDROME,REVERSE/index.js
+++ 05-COUNTDIGIT,PALINDROME,REVERSE/index.js
@@ -1,3 +1,6 @@
+/*
+Q)count how many digits??
+
 function countdigit(n){
     let count=0;
     if(n==0) return 1;
@@ -12,3 +15,26 @@ function countdigit(n){
 let num=-1110.3366;
 let results=countdigit(num);
 console.log(results);
+*/
+
+/*
+Q)check no is palindrome or not ??
+ 
+
+function ispalindrome(x){
+    let xcopy=x;
+    //handling the -ve no 
+    if(x<0) return false;
+    let rev=0;
+    while(x>0){
+       let rem=x%10;
+        rev=(rev*10)+rem;
+        x=Math.floor(x/10);
+    }
+    return xcopy===rev;
+}
+let num=525;
+let results=ispalindrome(num);
+console.log(results);
+*/
+
