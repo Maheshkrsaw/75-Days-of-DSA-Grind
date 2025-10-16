7890a870fddd63d725e637d0030625f82b38938d
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-16 20:21:52 +0530

    updatng

--- 07-REMOVE-DUPLICATE,ELEMENT/index.js
+++ 07-REMOVE-DUPLICATE,ELEMENT/index.js
@@ -18,3 +18,46 @@ let result = removeDuplicates(nums);
 console.log(result, nums);
 */
 
+
+
+
+
+
+
+
+
+// let nums = [3, 5, 11, 22, 2, 3 , 3, 3, 4];
+// let val =3;
+// function removElelemnt (val, nums){
+//   let x=0
+//   for(let i=0; i<nums.length;i++){
+//     if(nums[i]!==val){
+//       nums[x]=nums[i];
+//       x++;
+//     }
+//   }
+//   return x;
+// }
+
+// let result = removElelemnt(nums,val);
+// console.log(result, nums);
+
+
+
+let nums = [3,1,3,4,3,3,3,7];
+let val = 3;
+
+function removeElement(val, nums) {
+  let x = 0; // position for next non-val element
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != val) {
+      nums[x] = nums[i];
+      x++;
+    }
+  }
+  return x; // number of remaining elements
+}
+
+let result = removeElement(val, nums);
+console.log("New length:", result);
+console.log("Updated array:", nums.slice(0, result));
