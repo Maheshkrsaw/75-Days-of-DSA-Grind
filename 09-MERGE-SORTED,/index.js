b63e58054644b684315fe4f91d4229ccdf6807c8
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-20 09:51:41 +0530

    updated DAY 09

---
+++ 09-MERGE-SORTED,/index.js
@@ -0,0 +1,21 @@
+/*
+
+q) reverse the string
+
+let arr = ['h', 'e', 'l', 'l', 'o'];
+function reverseno(arr) {
+    // let start = 0;
+    let length=arr.length;
+    let half = Math.floor(arr.length/2);
+   for(i=0;i<half;i++){
+        let temp = arr[i];
+        arr[i] = arr[length-1-i];
+        arr[length-1-i] = temp;
+        
+    }
+    return arr;
+}
+console.log(reverseno(arr)); 
+*/
+
+
