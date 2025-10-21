682dd8b2d07ae2b3a712d64670dbcdecf332bce0
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-21 11:25:19 +0530

    updated DAY 09

--- 09-MERGE-SORTED,/index.js
+++ 09-MERGE-SORTED,/index.js
@@ -1,12 +1,12 @@
-let nums1 = [1, 10, 5,40,50];
-let nums2 = [2, 50, 8,50,80,60,50,40];
+// let nums1 = [1, 10, 5,40,50];
+// let nums2 = [2, 50, 8,50,80,60,50,40];
 
-// Step 1: append nums2 elements into nums1
-for (let i = 0; i < nums2.length; i++) {
-  nums1.push(nums2[i]);
-}
+// // Step 1: append nums2 elements into nums1
+// for (let i = 0; i < nums2.length; i++) {
+//   nums1.push(nums2[i]);
+// }
 
-// Step 2: sort in non-decreasing order (ascending)
-nums1.sort((a, b) => a - b);
+// // Step 2: sort in non-decreasing order (ascending)
+// nums1.sort((a, b) => a - b);
 
-console.log(nums1); // [1, 2, 2, 5, 5, 8]
+// console.log(nums1); // [1, 2, 2, 5, 5, 8]
