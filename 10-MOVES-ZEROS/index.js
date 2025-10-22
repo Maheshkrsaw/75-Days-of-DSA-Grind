cc286001a7e437d28b9fd2c8ceb126868a67cd58
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-22 10:38:27 +0530

    updated

--- 10-MOVES-ZEROS/index.js
+++ 10-MOVES-ZEROS/index.js
@@ -1,3 +1,8 @@
+/*
+// moves 0 in last 
+
+
+
 let nums=[0,1,0,3,12];
 
 function moves(nums){
@@ -15,4 +20,28 @@ return nums;
 }
 
 console.log(moves(nums));
-//[ 1, 3, 12, 0, 0 ]
+//[ 1, 3, 12, 0, 0 ] 
+
+
+*/
+
+
+
+
+
+let nums =[1,1,0,1,1];
+function consecutiveno(nums){
+    let currentCount=0;
+    let maxcount=0;
+    for(let i=0; i<nums.length;i++){
+        if(nums[i]==1){
+            currentCount++;
+        }else{
+            maxcount=Math.max(currentCount,maxcount);
+            currentCount=0;
+        }
+    }
+    return Math.max(currentCount,maxcount);
+}
+
+console.log(consecutiveno(nums));
