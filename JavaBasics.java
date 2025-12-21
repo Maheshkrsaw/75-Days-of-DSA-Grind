ea29040b0fba85d2dd1858807e7dda1f4271225e
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-12-21 18:28:14 +0530

    updating

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,10 +1,50 @@
+import java.util.*;
 public class JavaBasics{
-    
-    public static void Mahesh(){
-        System.out.println("MAhesh");
+    public static int CalculateSum(int a , int b){
+        int sum = a + b ;
+        System.out.println("sum is :- " + sum );
+        return sum;
     }
     public static void main(String[] args) {
-        // System.out.println("AMhesh");
-        Mahesh();
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter first no ");
+        int num1 = sc.nextInt();
+        System.out.print("enter second no ");
+        int num2 = sc.nextInt();
+        CalculateSum(num1,num2);
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+// import java.util.Scanner;
+
+// public class JavaBasics {
+
+//     public static int sum(int a, int b) {
+//         int sum = a + b;
+//         System.out.println("Sum is: " + sum);
+//         return sum;
+//     }
+
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter first number: ");
+//         int num1 = sc.nextInt();   // yahan input do
+
+//         System.out.print("Enter second number: ");
+//         int num2 = sc.nextInt();   // yahan input do
+
+//         sum(num1, num2);
+//     }
+// }
