7ee120d9f8962c3205e106389269037883122355
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-12-21 17:45:51 +0530

    updating portfolio

---
+++ JavaBasics.java
@@ -0,0 +1,10 @@
+public class JavaBasics{
+    public static void Helloworld(){
+        System.out.println("Mahesh");
+        System.out.println("Mahesh");
+        System.out.println("Mahesh");
+    }
+    public static void main(String[] args) {
+        Helloworld();
+    }
+}
