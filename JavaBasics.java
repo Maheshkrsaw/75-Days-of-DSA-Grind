63d24ca56dfcfe02494e1ff49d0a6ffec498853f
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-12-21 18:03:41 +0530

    updating

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,5 +1,10 @@
 public class JavaBasics{
+    
+    public static void Mahesh(){
+        System.out.println("MAhesh");
+    }
     public static void main(String[] args) {
-        System.out.println("AMhesh");
+        // System.out.println("AMhesh");
+        Mahesh();
     }
 }
