1942d480ce0865d442b997176f24579043136ca6
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-12-21 18:02:52 +0530

    updating

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,14 +1,5 @@
 public class JavaBasics{
-    public  int Helloworld(){
-        System.out.println("Mahesh");
-        System.out.println("Mahesh");
-        System.out.println("Mahesh");
-        return 1;
-    }
     public static void main(String[] args) {
-        System.out.println("Mahesh2");
-        JavaBasics obj = new JavaBasics();
-        obj.Helloworld();
-        // Helloworld();
+        System.out.println("AMhesh");
     }
 }
